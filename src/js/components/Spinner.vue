9cc24355473919dfdb88a7578d787209fb619800
paulschwoerer <kontakt@paulschwoerer.de>
2018-02-26 10:24:07 +0100

    Add spinners to Control buttons

---
+++ src/js/components/Spinner.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="spinner">
+        <svg viewBox="0 0 64 64">
+            <g>
+                <circle
+                        stroke-width="4"
+                        stroke-dasharray="100"
+                        stroke-dashoffset="82"
+                        stroke="currentColor"
+                        r="26"
+                        cx="32"
+                        cy="32"
+                        fill="none"
+                ></circle>
+            </g>
+        </svg>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Spinner',
+    };
+</script>
+
+<style lang="scss">
+    @import '../../scss/variables/colors';
+
+    .spinner {
+        color: $colorWhite;
+        width: 30px;
+        height: 30px;
+        animation: rotate 1s infinite linear;
+    }
+
+    @keyframes rotate {
+        from {
+            transform: rotate(0);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
+    }
+</style>
--- src/js/utils/socketUtils.js
+++ src/js/utils/socketUtils.js
@@ -4,14 +4,18 @@ export const asyncWebSocketRequest = (socket, requestType, data, responseType) =
   socket.send(`${requestType}/${data}`);
 
   return new Promise((resolve, reject) => {
-    socket.onmessage(({ data }) => {
-       const type = data.split('/')[0];
+    const onMessage = ({ data }) => {
+        const type = data.split('/')[0];
 
-       if (type === responseType) {
-           resolve(data);
-       } else if (type === SocketMessageTypes.ERROR) {
-           reject(data);
-       }
-    });
+        if (type === responseType) {
+            resolve(data);
+            socket.removeEventListener('message', onMessage);
+        } else if (type === SocketMessageTypes.ERROR) {
+            reject(data);
+            socket.removeEventListener('message', onMessage);
+        }
+    };
+
+    socket.addEventListener('message', onMessage);
   });
 };
