c1b9800530431f41556e8b753630f5fbd1cac515
Joseph Pal <joe.pal@gmx.net>
2019-03-07 15:20:52 +0100

    addded new socket message type for sending wlan configuration

--- src/js/enum/SocketMessageTypes.js
+++ src/js/enum/SocketMessageTypes.js
@@ -4,11 +4,13 @@ export default {
   START: 'start',
   STOP: 'stop',
   PAUSE: 'pause',
+  CONFIG: 'config',
 
   // server messages
   STOPPED: 'stopped',
   RUNNING: 'running',
   PAUSED: 'paused',
   READY: 'ready',
+  RECEIVED: 'received',
   ERROR: 'error',
 };
